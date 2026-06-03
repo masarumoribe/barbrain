82c554c9d60fc27b3e5eaf757e43eecae14cdcb9
Masaru Moribe <masaru.aya@gmail.com>
2026-06-03 20:26:56 +0900

    add Vercel URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://localhost:5174",
-        "https://barbrain-production.up.railway.app"],
+        "https://barbrain.vercel.app"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
