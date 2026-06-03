af0ba8438055f514d7f5795e3e34d2eeb4bf7df8
Masaru Moribe <masaru.aya@gmail.com>
2026-06-03 22:44:49 +0900

    Update API base URL to use environment variable

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { supabase } from '../lib/supabaseClient'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'http://127.0.0.1:8000',
 })
 
 api.interceptors.request.use(async (config) => {
